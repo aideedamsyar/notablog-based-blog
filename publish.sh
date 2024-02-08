12381132e5c471a7c4fbe95fb6a6e112f4f5ad52
Tengku Aideed Amsyar <82840182+aideedamsyar@users.noreply.github.com>
2024-02-08 18:53:27 +0900

    publish

--- publish.sh
+++ publish.sh
@@ -1,8 +1,24 @@
-#! /bin/bash
+#!/bin/bash
 
+# Generate site
 notablog generate .
+
+# Remove old 'docs' directory
 rm -rf docs
+
+# Copy 'public' to 'docs', preserving any existing files in 'docs'
 cp -r public docs
+
+# Check if CNAME exists in the 'docs' directory and preserve the custom domain
+if [ -f "./CNAME" ]; then
+    cp ./CNAME docs/CNAME
+fi
+
+# Add changes to git
 git add .
+
+# Commit changes
 git commit -m 'publish'
+
+# Push changes to the main branch
 git push origin main
